581092f23293650628c9572cebe994d747847ea5
Vyachelsav <dotabossnotbarboss@gmail.com>
2022-05-12 21:15:47 +0600

    a

--- src/js/script.js
+++ src/js/script.js
@@ -3,4 +3,23 @@ import Glide from "@glidejs/glide";
 ("use strict");
 document.addEventListener("DOMContentLoaded", () => {
   new Glide(".glide", { type: "carousel", startAt: 0, perView: 1 }).mount();
+
+  const submenuTriggers = document.querySelectorAll("[data-submenu-trigger]");
+
+  const classes = [
+    "submenu",
+    "submenu__inner",
+    "submenu__list",
+    "submenu__box",
+    "submenu__item",
+    "submenu__link",
+  ];
+
+  window.addEventListener("mousemove", (e) => {
+    submenuTriggers[0].classList.toggle(
+      "menu__list-link--active",
+      e.target.className.includes(...classes)
+    );
+  });
 });
+
