aeaf5263de452dd8ff8adbbeb8959e11e149ad22
Vyachelsav <dotabossnotbarboss@gmail.com>
2022-05-14 16:23:10 +0600

    f

--- gulpfile.js/task/scss.js
+++ gulpfile.js/task/scss.js
@@ -1,6 +1,5 @@
 const { src, dest } = require("gulp");
 
-const app = require("../config/app");
 const path = require("../config/path");
 // const csso = require("gulp-csso");
 const sass = require("gulp-sass")(require("sass"));
@@ -12,7 +11,7 @@ const gulpGroupCssMediaQueries = require("gulp-group-css-media-queries");
 
 const scss = () => {
   return (
-    src(path.scss.src, { sourcemaps: app.isDev })
+    src(path.scss.src)
       .pipe(sassGlob())
       .pipe(sass())
       .pipe(webpCss())
@@ -20,7 +19,7 @@ const scss = () => {
       .pipe(gulpGroupCssMediaQueries())
       // .pipe(csso())
       .pipe(concat("style.css"))
-      .pipe(dest(path.scss.dest, { sourcemaps: app.isDev }))
+      .pipe(dest(path.scss.dest))
   );
 };
 
