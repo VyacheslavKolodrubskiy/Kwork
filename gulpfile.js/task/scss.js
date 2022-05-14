c0c367e80d645e2fc2795048cb274f866c565cf2
Vyachelsav <dotabossnotbarboss@gmail.com>
2022-05-14 20:23:08 +0600

    f

--- gulpfile.js/task/scss.js
+++ gulpfile.js/task/scss.js
@@ -4,7 +4,7 @@ const path = require("../config/path");
 // const csso = require("gulp-csso");
 const sass = require("gulp-sass")(require("sass"));
 const concat = require("gulp-concat");
-const webpCss = require("gulp-webp-css");
+// const webpCss = require("gulp-webp-css");
 const sassGlob = require("gulp-sass-glob");
 const autoPrefixer = require("gulp-autoprefixer");
 const gulpGroupCssMediaQueries = require("gulp-group-css-media-queries");
@@ -14,7 +14,7 @@ const scss = () => {
     src(path.scss.src)
       .pipe(sassGlob())
       .pipe(sass())
-      .pipe(webpCss())
+      // .pipe(webpCss())
       .pipe(autoPrefixer())
       .pipe(gulpGroupCssMediaQueries())
       // .pipe(csso())
