884275d1899fce7b569d643bb72e81c7e222fdd1
Vyachelsav <dotabossnotbarboss@gmail.com>
2022-05-14 18:54:56 +0600

    f

--- src/js/script.js
+++ src/js/script.js
@@ -2,5 +2,16 @@ import Glide from "@glidejs/glide";
 
 ("use strict");
 document.addEventListener("DOMContentLoaded", () => {
-  new Glide(".glide", { type: "carousel", startAt: 0, perView: 1 }).mount();
+  new Glide(".glide", {
+    type: "carousel",
+    startAt: 0,
+    perView: 1,
+  }).mount();
+
+  new Glide(".industrial__slider", {
+    type: "carousel",
+    startAt: 0,
+    perView: 3,
+  }).mount();
 });
+
